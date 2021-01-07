774217635d3ffa02dc2ba3e9253f1dd2af3fa5fb
Deepak Mahakale <deepakmahakale@gmail.com>
2021-01-07 22:26:20 +0530

    User validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # let(:user) { create(:user) }
+
+  context '#validations' do
+    it 'username: minimum 1 character' do
+      user = build(:user, username: '')
+      expect(user.valid?).to be false
+      byebug
+      expect(user.errors.messages[:username]).to include('is too short (minimum is 1 character)')
+    end
+
+    it 'username: only alphanumeric characters and underscore' do
+      user = build(:user, username: 'user+$')
+      expect(user.valid?).to be false
+      expect(user.errors.messages[:username]).to include('Only alphanumeric characters and _ is allowed')
+    end
+
+    it 'username: should be unique' do
+      create(:user, username: 'username123')
+      user2 = build(:user, username: 'username123')
+
+      expect(user2.valid?).to be false
+      expect(user2.errors.messages[:username]).to include('has already been taken')
+    end
+  end
 end
