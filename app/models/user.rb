59b48cc4c2b030a706ece57d6384e4694a411779
Deepak Mahakale <deepakmahakale@gmail.com>
2021-01-07 22:26:35 +0530

    Changes in model to pass test cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true,
+                       length: { minimum: 1 },
+                       uniqueness: true,
+                       format: { with: /\A[A-Za-z0-9_]+\Z/, message: 'Only alphanumeric characters and _ is allowed' }
 end
