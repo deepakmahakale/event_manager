0f43a8b84dbac997af2c0a089a070e94f46548ee
Deepak Mahakale <deepakmahakale@gmail.com>
2021-01-09 09:57:43 +0530

    Add devise

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,12 @@ ActiveRecord::Schema.define(version: 2021_01_02_170449) do
     t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
